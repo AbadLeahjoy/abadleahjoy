f450f4f7bd6a7eb7ba4abe5772a327142f5e9fcf
AbadLeahjoy <168160848+AbadLeahjoy@users.noreply.github.com>
2024-04-26 12:50:35 +0800

    Add files via upload

---
+++ numbers/script.js
@@ -0,0 +1,24 @@
+
+function sumThreeNumbers(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+
+function differenceTwoNumbers(num1, num2) {
+    return num1 - num2;
+}
+
+
+function productTwoNumbers(num1, num2) {
+    return num1 * num2;
+}
+
+
+function averageTwoNumbers(num1, num2) {
+    return (num1 + num2) / 2;
+}
+
+console.log(sumThreeNumbers(5, 20, 20));  
+console.log(differenceTwoNumbers(20, 5));  
+console.log(productTwoNumbers(5, 5));      
+console.log(averageTwoNumbers(30, 50));    
